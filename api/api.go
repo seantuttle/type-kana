efd2e0362a066ada8cc4e62939adac2e17bb6f9f
tuttlsj1 <sean.j.tuttle@vanderbilt.edu>
2022-03-04 22:33:30 -0600

    update api

--- api/api.go
+++ api/api.go
@@ -3,32 +3,17 @@ package api
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
-	"time"
 )
 
-var templates = template.Must(template.ParseGlob("../templates/*"))
-
-func logging(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		next.ServeHTTP(w, r)
-		log.Println(r.Method, r.URL, "handled in", time.Since(start))
-	})
+func Index(w http.ResponseWriter, r *http.Request) {
+	err := template.Must(template.ParseGlob("../templates/*")).ExecuteTemplate(w, "index", nil)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("index: couldn't parse template: %v", err), http.StatusInternalServerError)
+		return
+	}
 }
 
-// index is the handler responsible for rending the index page for the site.
-func Index() http.Handler {
-	return logging(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		err := templates.ExecuteTemplate(w, "index", nil)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("index: couldn't parse template: %v", err), http.StatusInternalServerError)
-			return
-		}
-	}))
-}
-
-func Static() http.Handler {
-	return logging(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
-}
+// func Static() http.Handler {
+// 	return logging(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+// }
